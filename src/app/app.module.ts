57effc77df394bb76eae43a28df3b5cc98368e37
choksi-krishna <krishna.choksi@bacancytechnology.com>
2020-01-09 19:34:54 +0530

    checkbox select all/unselect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckBoxComponent } from './check-box/check-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
